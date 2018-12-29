9f4b88701d2c1581b5395007b721397aa3e5ee4b
Manuel <manuel.coffin@gmail.com>
2018-12-29 19:16:06 +0100

    Updated BaseUrl

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production'
+      ? '/dist/'
+      : '/',
   lintOnSave: false,
   chainWebpack: config => {
     const oneOfsMap = config.module.rule('scss').oneOfs.store;
